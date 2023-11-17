d67980c1bbb2c83aebd7fb89ab6c13549770d117
amelnikov <amelnikov@PC100127>
2023-11-17 14:15:34 +0700

    bounding methods added

--- FirstRevitPlugin/Class1.cs
+++ FirstRevitPlugin/Class1.cs
@@ -5,24 +5,109 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB.Architecture;
 
 namespace FirstRevitPlugin
 {
-    [Autodesk.Revit.Attributes.TransactionAttribute(Autodesk.Revit.Attributes.TransactionMode.Manual)]
-    public class FirstRevitCommand : IExternalCommand
+    [TransactionAttribute(TransactionMode.Manual)]
+    [RegenerationAttribute(RegenerationOption.Manual)]
+    public class Lab1PlaceGroup : IExternalCommand
     {
-        static AddInId addinId = new AddInId(new Guid("2421F661-DB8D-4D7F-A005-EBA8BA15BFD7"));
-        public Result Execute(ExternalCommandData commandData)
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            UIDocument uidoc = commandData.Application.ActiveUIDocument;
-            Document doc = uidoc.Document;
-            Reference myRef = uidoc.Selection.PickObject(ObjectType.Element, "Выберите элемент для вывода его Id");
-            Element element = doc.GetElement(myRef);
-            ElementId id = element.Id;
-            TaskDialog.Show("Hello world!", id.ToString());
-            var view = new UserControl1();
-            view.ShowDialog();
+            //Получение объектов приложения и документа
+            UIApplication uiApp = commandData.Application;
+            Document doc = uiApp.ActiveUIDocument.Document;
+            //Определение объекта-ссылки для занесения результата указания
+            try
+            {
+                Reference pickedRef = null;
+                //Указание группы
+                Selection sel = uiApp.ActiveUIDocument.Selection;
+                GroupPickFilter selFilter = new GroupPickFilter();
+                pickedRef = sel.PickObject(ObjectType.Element, selFilter, "Выберите группу");
+                Element elem = doc.GetElement(pickedRef);
+                Group group = elem as Group;
+                XYZ origin = GetElementCenter(group);
+                Room room = GetRoomOfGroup(doc, origin);
+                //Указание точки
+                XYZ sourceCenter = GetRoomCenter(room);
+                XYZ groupLocation = sourceCenter + new XYZ(13.12, 0, 0);
+
+                string coords =
+                 "X = " + sourceCenter.X.ToString() + "\r\n" +
+                 "Y = " + sourceCenter.Y.ToString() + "\r\n" +
+                 "Z = " + sourceCenter.Z.ToString();
+                TaskDialog.Show("Центр исходной комнаты", coords);
+                //Размещение группы
+                Transaction trans = new Transaction(doc);
+                trans.Start("Lab");
+                doc.Create.PlaceGroup(sourceCenter, group.GroupType);
+                trans.Commit();
+            }
+            catch (Autodesk.Revit.Exceptions.OperationCanceledException)
+            {
+                return Result.Cancelled;
+            }
+
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+            }
+
             return Result.Succeeded;
         }
+        public XYZ GetElementCenter(Element elem)
+        {
+            BoundingBoxXYZ bounding = elem.get_BoundingBox(null);
+            XYZ center = (bounding.Max + bounding.Min) * 0.5;
+            return center;
+        }
+        Room GetRoomOfGroup(Document doc, XYZ point)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfCategory(BuiltInCategory.OST_Rooms);
+            Room room = null;
+            foreach (Element elem in collector)
+            {
+                room = elem as Room;
+                if (room != null)
+                {
+                    // Точка в указанной комнате?
+                    if (room.IsPointInRoom(point))
+                    {
+                        break;
+                    }
+                }
+            }
+            return room;
+        }
+        public XYZ GetRoomCenter(Room room)
+        {
+            // Получение центра комнаты
+            XYZ boundCenter = GetElementCenter(room);
+            LocationPoint locPt = (LocationPoint)room.Location;
+            XYZ roomCenter =
+            new XYZ(boundCenter.X, boundCenter.Y, locPt.Point.Z);
+            return roomCenter;
+        }
+
     }
+    public class GroupPickFilter : ISelectionFilter
+    {
+        public bool AllowElement(Element e)
+        {
+            return (e.Category.Id.IntegerValue.Equals(
+            (int)BuiltInCategory.OST_IOSModelGroups));
+        }
+        public bool AllowReference(Reference r, XYZ p)
+        {
+            return false;
+        }
+    }
+
 }
--- FirstRevitPlugin/FirstRevitPlugin1.cs
+++ FirstRevitPlugin/FirstRevitPlugin1.cs
@@ -1,11 +1,1 @@
-﻿<RevitAddIns>
-<AddIn Type="Command">
-<Name>FirstRevitCommand</Name>
-<Assembly>FirstRevitPlugin.dll</Assembly>
-<AddInId>2421F661-DB8D-4D7F-A005-EBA8BA15BFD7</AddInId>
-<FullClassName>FirstRevitPlugin.FirstRevitCommand</FullClassName>
-<Text>Моя первая команда</Text>
-<VendorId>MyId</VendorId>
-<VendorDescription></VendorDescription>
-</AddIn>
-</RevitAddIns>
+﻿
